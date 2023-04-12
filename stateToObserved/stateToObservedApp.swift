d31dcd77c960dc5af534b1b555f07dd9d61c0371
Emiliano Medina Delgado <emimed.04@gmail.com>
2023-04-12 12:16:25 -0600

    Initial Commit

---
+++ stateToObserved/stateToObservedApp.swift
@@ -0,0 +1,17 @@
+//
+//  stateToObservedApp.swift
+//  stateToObserved
+//
+//  Created by Facultad de Contaduría y Administración on 12/04/23.
+//
+
+import SwiftUI
+
+@main
+struct stateToObservedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
